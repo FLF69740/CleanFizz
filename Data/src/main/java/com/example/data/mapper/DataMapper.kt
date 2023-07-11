53bc6e83ab2e21012fa7511d3c4deaa53b73c109
LE FORT François <slayer171.flf@gmail.com>
2023-07-11 16:07:13 +0200

    Get saved data with viewModel

---
+++ Data/src/main/java/com/example/data/mapper/DataMapper.kt
@@ -0,0 +1,16 @@
+package com.example.data.mapper
+
+import com.example.core.model.FormularyBusinessModel
+
+object DataMapper {
+    /*
+    fun frontNextScreenToBusiness(formulary: FormularyBusinessModel): FormularyPreferences = FormularyPreferences(
+        wordOne = formulary.wordOne,
+        wordTwo = formulary.wordTwo,
+        numberOne = formulary.numberOne,
+        numberTwo = formulary.numberTwo,
+        limit = formulary.limit
+    )
+
+     */
+}
--- app/src/main/java/com/example/cleanfizz/ErrorFragment.kt
+++ app/src/main/java/com/example/cleanfizz/ErrorFragment.kt
@@ -5,12 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import com.example.cleanfizz.viewmodel.MainViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class ErrorFragment : Fragment() {
+
+    private val viewModel: MainViewModel by viewModel()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_error, container, false)
 
+        val title  = view.findViewById<TextView>(R.id.error_text_title)
 
         return view
     }
