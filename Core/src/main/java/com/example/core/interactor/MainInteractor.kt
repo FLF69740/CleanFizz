e141815cdbd2980fd03992e18a3c8e09389b79a6
LE FORT François <slayer171.flf@gmail.com>
2023-07-12 09:41:38 +0200

    Create fizz buzz list use case

--- Core/src/main/java/com/example/core/interactor/MainInteractor.kt
+++ Core/src/main/java/com/example/core/interactor/MainInteractor.kt
@@ -2,8 +2,10 @@ package com.example.core.interactor
 
 import com.example.core.usecase.GetErrorMessageUseCase
 import com.example.core.usecase.GetNextScreenAfterFormularyUseCase
+import com.example.core.usecase.GetFizzBuzzListUseCase
 
 data class MainInteractor (
     val getNexScreen: GetNextScreenAfterFormularyUseCase,
-    val getErrorMessage: GetErrorMessageUseCase
+    val getErrorMessage: GetErrorMessageUseCase,
+    val getFizzBuzzList: GetFizzBuzzListUseCase
 )
